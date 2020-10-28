c1cf2847e635142fd28c9dc4a638654841780dca
Saul Belisle <rbelisle1270@gmail.com>
2020-10-27 19:14:28 -0700

    fixing pic column width

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,11 +3,15 @@ class NotificationsController < ApplicationController
 
     def index
         @user = current_user
-        @notifications = @user.notifications.all.order("created_at DESC")
+        @notifications = Notification.where(recipient: current_user).unread
         #@notification = Notification.find(params[:id])
         #@actor = User.find(params[id: @notification.actor_id])
     end
 
+    def mark_as_read
+        @notifications = Notification.where(recipient: current_user).unread
+        @notifcation.update(read_at: Time.zone.now)
+    end
 
 
 end
