f73e9f8073875e3202d217d3e007d6b24b61d030
Saul Belisle <rbelisle1270@gmail.com>
2020-10-30 15:00:59 -0700

    timeline only shows friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
   
   end
 
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
