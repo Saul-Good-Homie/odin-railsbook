9c3d1a95b8118dd963d3ab4dbfe1fa837929026d
Saul Belisle <rbelisle1270@gmail.com>
2020-10-30 15:23:09 -0700

    send friend request on new user sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
     default from: 'Railsbooks@example.com'
 
-    def welcome_email
-        @user = params[:user]
+    def welcome_email(user)
+        @user = user
         @url  = 'http://example.com/login'
         mail(to: @user.email, subject: 'Welcome to Railsbook, a Facebook clone!')
     end
