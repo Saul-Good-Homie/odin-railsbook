61f6301b2e890fd877c1217d1be87d21de049727
Saul Belisle <rbelisle1270@gmail.com>
2020-11-02 14:35:42 -0800

    heroku troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+#devise default URL
+# ActionMailer Config
+config.action_mailer.default_url_options = { :host => ‘https://stormy-springs-25084.herokuapp.com/'}
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
